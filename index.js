255641b3ecf491445563719038ca5fad14ef64fe
Dexter <hannahsolmorgan@gmail.com>
2022-11-12 13:29:45 -0500

    Added functions to-do-list

--- index.js
+++ index.js
@@ -9,3 +9,80 @@ let Database = []
 let lastPassword
 
 let minimumPasswordLength = 7
+
+// 1. Check if the input lengths are valid
+
+
+// Sign Up
+
+// Check if they are already in the database
+// Check if username collide with another user
+
+// If none of those then sign them up
+// aka put them in the Database
+
+
+// Log In
+
+function resetInputfields() {
+   username_input.value = ""
+   password_input.value = ""
+}
+
+function checkLengthOfInputs() {
+   if (password_input.value.length <= minimumPasswordLength || username_input.value.length <= 0) {
+
+      if (username_input.value.length <= 0) {
+         alert("Your username is too short.")
+      } else {
+         alert("Your password is too short.")
+         resetInputfields()
+      }
+
+      return false
+
+   } else {
+      return true
+   }
+}
+
+function addUserDataToDatabase(username,password) {
+   let newUserData = {Username : username, Password : password}
+   Database.push(newUserData)
+   console.log(Database)
+   resetInputfields()
+   alert("You have successfully signed up! You may now login.")
+}
+
+function checkIfAlreadyInDatabase(username,password,buttonClicked) {
+
+   for (userData in Database) {
+
+      if (buttonClicked == "signUp") {
+
+         if (username.toLowerCase() == Database[userData].Username.toLowerCase()) {
+            alert(`A user by the name '${username}', seems to already be in our database. Try again.`)
+            resetInputfields()
+            break
+         } else {
+            addUserDataToDatabase(username,password)
+            console.log("Not in database")
+            break
+         }
+
+      } else {
+         if (username.toLowerCase() == Database[userData].Username.toLowerCase()) {
+            if (password == Database[userData].Password) {
+               alert("You have sucessfully logged into your account.")
+               resetInputfields()
+            }
+         }
+      }
+
+   }
+
+   if (Database.length <= 0 && buttonClicked == "signUp") { 
+      addUserDataToDatabase(username,password)
+      console.log("No need to check, the database is empty!")
+   }
+}
