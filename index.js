3501b681c5523fe449019c947e329c5957431dfd
Dexter <hannahsolmorgan@gmail.com>
2022-11-12 13:28:59 -0500

    1st part

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+let signupButton = document.getElementsByClassName("signupButton")[0]
+let loginButton = document.getElementsByClassName("loginButton")[0]
 
+let username_input = document.getElementsByClassName("user")[0]
+let password_input = document.getElementsByClassName("pass")[0]
+
+let Database = []
+
+let lastPassword
+
+let minimumPasswordLength = 7
